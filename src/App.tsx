74c5a46b49d0f5c74ffde35d07780113ed7cdf45
DADA YO <dayeon.yoon@gmail.com>
2022-09-15 21:21:55 +0900

    STYLE: Set theme, globalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { GlobalStyle } from "./style/globalStyle";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./style/theme";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "./atoms";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  const isDark = useRecoilValue(isDarkAtom);
+
+  return (
+    <>
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
